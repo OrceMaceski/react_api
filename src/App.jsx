74c548f0512045b9bdac9af4866b574ef7dc53f1
orcemaceski <maceskiorce@gmail.com>
2023-12-14 15:30:19 +0100

    Vite react app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import PostsList from './features/posts/PostsList'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='app'>
+        <h1>React on Rails</h1>
+        <p>Find this application layout in src/App.js</p>
+        <PostsList />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/features/posts/PostsList.jsx
@@ -0,0 +1,42 @@
+//API comes from .env.development file
+import React, { useState, useEffect } from 'react';
+import { API_URL } from '../../constants';
+
+function PostsList() {
+  const [posts, setPosts] = useState([]);
+  const [,setLoading] = useState(true);
+  const [,setError] = useState(null);
+
+// Fetch posts frm the API
+useEffect(() => {
+  async function loadPosts() {
+    try {
+      const response = await fetch(API_URL);
+      if (response.ok) {
+        const json = await response.json();
+        setPosts(json);
+      } else {
+          throw response;
+      }
+    } catch (error) {
+      setError("An error occurred. Awkward...");
+      console.log("An error occurred:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  loadPosts();
+}, []);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id} className="post-container">
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+export default PostsList;
